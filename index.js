1adb32572fb90dc93cd95d014b598022270547d2
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-09-05 11:09:51 +0200

    Updates Today

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(
     cors()
 )
 app.get('^/$|/BellaItalia', (req, res) => {
-    res.status(200).sendFile(path.resolvegit ('./static/index.html'))
+    res.status(200).sendFile(path.resolve('./static/index.html'))
 })
 // app.use('/^')
 // app.use('^/$')
